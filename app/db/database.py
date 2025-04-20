bfa38460485337dfc41651a87e2a3ecbb6ab4c07
Ezgi <ezgiekinergun@gmail.com>
2025-04-20 21:17:20 +0300

    Add Story model

---
+++ app/db/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# ENV değişkenlerinden değerleri al
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+# SQLAlchemy setup
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, BigInteger
+from db.database import Base
+
+class Story(Base):
+    __tablename__ = "stories"
+
+    id = Column(Integer, primary_key=True, index=True)  # Story ID
+    title = Column(String, nullable=False)
+    score = Column(Integer)
+    url = Column(String)
+    author = Column(String, index=True)
+    time = Column(BigInteger)
+    descendants = Column(Integer)  # Yorum sayısı
+    type = Column(String)
