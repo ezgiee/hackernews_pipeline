cc457a42ba9a4ba07a53c157427e86d9a5f87fe5
Ezgi <ezgiekinergun@gmail.com>
2025-04-21 07:58:04 +0300

    Minor change

--- app/celeryconfig.py
+++ app/celeryconfig.py
@@ -3,6 +3,6 @@ from celery.schedules import crontab
 beat_schedule = {
     "fetch-top-stories-every-hour": {
         "task": "app.tasks.hn_tasks.fetch_top_stories",
-        "schedule": crontab(minute=15, hour="*"),
+        "schedule": crontab(minute=0, hour="*"),
     },
 }
--- app/models.py
+++ app/models.py
@@ -4,11 +4,9 @@ from app.db.database import Base
 class Story(Base):
     __tablename__ = "stories"
 
-    id = Column(Integer, primary_key=True, index=True)  # Story ID
     title = Column(String, nullable=False)
     score = Column(Integer)
     url = Column(String)
     author = Column(String, index=True)
     time = Column(BigInteger)
-    descendants = Column(Integer)  # Yorum sayısı
     type = Column(String)
